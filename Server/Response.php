d1124fc0611c18da5c45a7f1f60bd582a9555ef6
Marco Pivetta <ocramius@gmail.com>
2014-08-12 14:02:47 +0200

    Importing state as of zendframework/zf2@b2f7d6777b33a1a3a70a9a1fee766e642f6a6ff3 (1396564873)
    Automatic import via rsync

--- Server/Response.php
+++ Server/Response.php
@@ -84,11 +84,9 @@ class Response
     public function loadJson($json)
     {
         $options = Json::decode($json, Json::TYPE_ARRAY);
-
         if (!is_array($options)) {
             throw new Exception\RuntimeException('json is not a valid response; array expected');
         }
-
         $this->setOptions($options);
     }
 
