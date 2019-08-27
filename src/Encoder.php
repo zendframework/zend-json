205e0a84e94d306bd9ef31cc20dff5fc01095068
webimpress <contact@webimpress.com>
2019-08-27 22:58:00 +0100

    Fix: replace curly offset access brace with square brackets
    As of PHP 7.4 using curly brackets is deprecated.
    
    Fixes #45

--- src/Encoder.php
+++ src/Encoder.php
@@ -585,14 +585,14 @@ class Encoder
             case 2:
                 // Return a UTF-16 character from a 2-byte UTF-8 char;
                 // see: http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
-                return chr(0x07 & (ord($utf8{0}) >> 2)) . chr((0xC0 & (ord($utf8{0}) << 6)) | (0x3F & ord($utf8{1})));
+                return chr(0x07 & (ord($utf8[0]) >> 2)) . chr((0xC0 & (ord($utf8[0]) << 6)) | (0x3F & ord($utf8[1])));
 
             case 3:
                 // Return a UTF-16 character from a 3-byte UTF-8 char;
                 // see: http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
-                return chr((0xF0 & (ord($utf8{0}) << 4))
-                    | (0x0F & (ord($utf8{1}) >> 2))) . chr((0xC0 & (ord($utf8{1}) << 6))
-                    | (0x7F & ord($utf8{2})));
+                return chr((0xF0 & (ord($utf8[0]) << 4))
+                    | (0x0F & (ord($utf8[1]) >> 2))) . chr((0xC0 & (ord($utf8[1]) << 6))
+                    | (0x7F & ord($utf8[2])));
         }
 
         // ignoring UTF-32 for now, sorry
